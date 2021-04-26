44e5256bd5dc2cf8376eaf149d0302256d137279
Jason Zhang <jasonzhang@192-168-1-105.tpgi.com.au>
2021-04-26 10:09:57 +1000

    style: adjust styles

--- components/Author.js
+++ components/Author.js
@@ -1,18 +1,24 @@
 import { Avatar, Divider } from 'antd';
-import { GithubOutlined, QqOutlined, WechatOutlined } from '@ant-design/icons';
+import { GithubFilled, LinkedinFilled, FacebookFilled } from '@ant-design/icons';
 
-import styles from '../styles/Author.module.css';
+import styles from '../public/styles/Author.module.css';
 
 const Author = () => {
   return (
     <div className={styles.layout}>
       <Avatar className={styles.img} size={100} src="https://www.shareicon.net/data/512x512/2016/08/18/814068_face_512x512.png" />
       <div className={styles.summary}>
-        Good things are free, such as water, sunlight and air. So all the videos on this site are free. Welcome to the blog.
+        Good things are free, such as water, sunlight and air. So all tutorials on this site are free. Welcome to my blog.
         <Divider>Social Media</Divider>
-        <Avatar size={28} icon={<GithubOutlined />} className={styles.account} />
-        <Avatar size={28} icon={<QqOutlined />} className={styles.account} />
-        <Avatar size={28} icon={<WechatOutlined />} className={styles.account} />
+        <a href="https://github.com/jasonzhang7100" rel="noopener noreferrer" target="_blank">
+          <GithubFilled style={{ fontSize: "1.8rem" }} />
+        </a>
+        <a href="https://www.linkedin.com/in/jason-zhang-syd/" rel="noopener noreferrer" target="_blank">
+          <LinkedinFilled style={{ fontSize: "1.8rem" }} />
+        </a>
+        <a href="https://www.facebook.com/" rel="noopener noreferrer" target="_blank">
+          <FacebookFilled style={{ fontSize: "1.8rem" }} />
+        </a>
       </div>
     </div>
   )
--- components/Header.js
+++ components/Header.js
@@ -3,24 +3,27 @@ import Link from 'next/link';
 import Router from 'next/router';
 import axios from 'axios';
 import { Row, Col, Menu } from 'antd';
-import { Icon } from '@ant-design/compatible';
+import { createFromIconfontCN } from '@ant-design/icons';
 
-import styles from '../styles/Header.module.css';
+import styles from '../public/styles/Header.module.css';
 import servicePath from '../config/apiUrl';
 
+const IconFont = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_2509173_ilhw378q9br.js',
+});
+
 const Header = () => {
+
   const [navArray, setNavArray] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(servicePath.getTypeInfo).then(
-        res => res.data.data
-      );
-      setNavArray(result);
-    };
+  const fetchData = async () => {
+    const result = await axios(servicePath.getTypeInfo).then(
+      res => res.data.data
+    );
+    setNavArray(result);
+  };
 
-    fetchData();
-  }, []);
+  useEffect(() => fetchData(), []);
 
   const handleClick = e => {
     if (e.key == 0) {
@@ -33,24 +36,21 @@ const Header = () => {
   return (
     <Row className={styles.wrapper}>
       <Col xs={24} sm={24} md={10} lg={12} xl={15}>
-        <span className={styles.logo}>
-          <Link href={{ pathname: '/' }}>
-            JZblog
-          </Link>
-        </span>
-        <span className={styles.logo_text}>focusing on frontend stack</span>
+        <Link href={{ pathname: '/' }}>
+          <img src="../static/JZblog_logo.png" alt="logo" className={styles.logo} />
+        </Link>
       </Col>
 
       <Col xs={0} sm={0} md={14} lg={12} xl={9}>
         <Menu mode="horizontal" onClick={handleClick}>
           <Menu.Item key="0">
-            <Icon type="home" />Home
+            <IconFont type="icon-home" />Home
           </Menu.Item>
 
           {
             navArray.map(item => (
               <Menu.Item key={item.id}>
-                <Icon type={item.icon} />
+                <IconFont type={item.icon} />
                 {item.typeName}
               </Menu.Item>
             ))
